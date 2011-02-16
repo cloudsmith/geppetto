0fd8a1ccdb6d8b63f02b38385137e2492e9889a0
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-02-16 14:57:16 +0100

    Made diagnostic IDs globally unique.

--- org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/validation/IPPDiagnostics.java
+++ org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/validation/IPPDiagnostics.java
@@ -17,69 +17,71 @@ package org.cloudsmith.geppetto.pp.dsl.validation;
  * 
  */
 public interface IPPDiagnostics {
-	public static final String ISSUE__RESOURCE_BAD_TYPE_FORMAT = "Bad Resource Type Format";
+	public static final String ISSUE_PREFIX = "org.cloudsmith.geppetto.pp.dsl.validation.issue.";
 
-	public static final String ISSUE__RESOURCE_UNKNOWN_TYPE = "Unknown Resource Type";
+	public static final String ISSUE__RESOURCE_BAD_TYPE_FORMAT = ISSUE_PREFIX + "BadResourceTypeFormat";
 
-	public static final String ISSUE__RESOURCE_MULTIPLE_BODIES = "Resouce Multiple Bodies";
+	public static final String ISSUE__RESOURCE_UNKNOWN_TYPE = ISSUE_PREFIX + "UnknownResourceType";
 
-	public static final String ISSUE__RESOURCE_WITH_TITLE = "Resource Default With Title";
+	public static final String ISSUE__RESOURCE_MULTIPLE_BODIES = ISSUE_PREFIX + "ResouceMultipleBodies";
 
-	public static final String ISSUE__RESOURCE_WITHOUT_TITLE = "Resource Without Title";
+	public static final String ISSUE__RESOURCE_WITH_TITLE = ISSUE_PREFIX + "ResourceDefaultWithTitle";
 
-	public static final String ISSUE__RESOURCE_WITH_ADDITIONS = "Resource With Attribute Additions";
+	public static final String ISSUE__RESOURCE_WITHOUT_TITLE = ISSUE_PREFIX + "ResourceWithoutTitle";
 
-	public static final String ISSUE__RESOURCE_REFERENCE_NO_REFERENCE = "Resource Override No Reference";
+	public static final String ISSUE__RESOURCE_WITH_ADDITIONS = ISSUE_PREFIX + "ResourceWithAttributeAdditions";
 
-	public static final String ISSUE__RESOURCE_OVERRIDE_BAD_REFERENCE = "Resource Override Bad Reference";
+	public static final String ISSUE__RESOURCE_REFERENCE_NO_REFERENCE = ISSUE_PREFIX + "ResourceOverrideNoReference";
 
-	public static final String ISSUE__NOT_CLASSREF = "Not Class Reference";
+	public static final String ISSUE__RESOURCE_OVERRIDE_BAD_REFERENCE = ISSUE_PREFIX + "ResourceOverrideBadReference";
 
-	public static final String ISSUE__DEPRECATED_REFERENCE = "Deprecated Reference";
+	public static final String ISSUE__NOT_CLASSREF = ISSUE_PREFIX + "NotClassReference";
 
-	public static final String ISSUE__RESOURCE_REFERENCE_NO_PARAMETERS = "Resource Reference No Parameters";
+	public static final String ISSUE__DEPRECATED_REFERENCE = ISSUE_PREFIX + "DeprecatedReference";
 
-	public static final String ISSUE__RESOURCE_NOT_VIRTUALIZEABLE = "Not Virtualizeable";
+	public static final String ISSUE__RESOURCE_REFERENCE_NO_PARAMETERS = ISSUE_PREFIX + "ResourceReferenceNoParameters";
+
+	public static final String ISSUE__RESOURCE_NOT_VIRTUALIZEABLE = ISSUE_PREFIX + "NotVirtualizeable";
 
 	/**
 	 * The given type of expression is not supported at the given place.
 	 */
-	public static final String ISSUE__UNSUPPORTED_EXPRESSION = "Unsupported Expression";
+	public static final String ISSUE__UNSUPPORTED_EXPRESSION = ISSUE_PREFIX + "UnsupportedExpression";
 
 	/**
 	 * An expression feature that is optional in the grammar/model is not optional in the concrete syntax.
 	 */
-	public static final String ISSUE__REQUIRED_EXPRESSION = "Required Expression";
+	public static final String ISSUE__REQUIRED_EXPRESSION = ISSUE_PREFIX + "RequiredExpression";
 
-	public static final String ISSUE__NOT_NAME = "Not Name Compliant";
+	public static final String ISSUE__NOT_NAME = ISSUE_PREFIX + "NotNameCompliant";
 
-	public static final String ISSUE__NOT_STRING = "Not String Compliant";
+	public static final String ISSUE__NOT_STRING = ISSUE_PREFIX + "NotStringCompliant";
 
-	public static final String ISSUE__NOT_NAME_OR_REF = "Neither Name Nor Reference";
+	public static final String ISSUE__NOT_NAME_OR_REF = ISSUE_PREFIX + "NeitherNameNorReference";
 
 	/**
 	 * This should not happen if a model is parsed from text, but can occur when manually constructing a model.
 	 */
-	public static final String ISSUE__ILLEGAL_OP = "Illegal Operator";
+	public static final String ISSUE__ILLEGAL_OP = ISSUE_PREFIX + "IllegalOperator";
 
-	public static final String ISSUE__NOT_REGEX = "Badly formed regular expression";
+	public static final String ISSUE__NOT_REGEX = ISSUE_PREFIX + "Badlyformedregularexpression";
 
-	public static final String ISSUE__NOT_ASSIGNABLE = "Not Assignable";
+	public static final String ISSUE__NOT_ASSIGNABLE = ISSUE_PREFIX + "NotAssignable";
 
-	public static final String ISSUE__RESERVED_WORD = "Reserved Word";
+	public static final String ISSUE__RESERVED_WORD = ISSUE_PREFIX + "ReservedWord";
 
-	public static final String ISSUE__NOT_VARNAME = "Not Variable Name";
+	public static final String ISSUE__NOT_VARNAME = ISSUE_PREFIX + "NotVariableName";
 
-	public static final String ISSUE__NOT_APPENDABLE = "Not Appendable";
+	public static final String ISSUE__NOT_APPENDABLE = ISSUE_PREFIX + "NotAppendable";
 
-	public static final String ISSUE__NULL_EXPRESSION = "Null Expression";
+	public static final String ISSUE__NULL_EXPRESSION = ISSUE_PREFIX + "NullExpression";
 
-	public static final String ISSUE__NOT_TOPLEVEL = "Not Top Level Expression";
+	public static final String ISSUE__NOT_TOPLEVEL = ISSUE_PREFIX + "NotTopLevelExpression";
 
-	public static final String ISSUE__UNKNOWN_FUNCTION_REFERENCE = "Reference to unknown function";
+	public static final String ISSUE__UNKNOWN_FUNCTION_REFERENCE = ISSUE_PREFIX + "ReferenceToUnknownfunction";
 
-	public static final String ISSUE__NOT_RVALUE = "Not a Right Value";
+	public static final String ISSUE__NOT_RVALUE = ISSUE_PREFIX + "NotRightValue";
 
-	public static final String ISSUE__UNSUPPORTED_REGEX_FLAGS = "Unsupported Regex Flags";
+	public static final String ISSUE__UNSUPPORTED_REGEX_FLAGS = ISSUE_PREFIX + "UnsupportedRegexFlags";
 
 }

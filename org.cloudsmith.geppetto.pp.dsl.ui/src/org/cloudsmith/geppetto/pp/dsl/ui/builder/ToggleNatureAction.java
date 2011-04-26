4daa0dafbd72dd197ada4a4d9c40f873b54f695f
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-04-26 02:10:46 +0200

    Made adding and removing the Puppet Project Nature easier to use. Closes #5. Closes #45.

--- org.cloudsmith.geppetto.pp.dsl.ui/src/org/cloudsmith/geppetto/pp/dsl/ui/builder/ToggleNatureAction.java
+++ org.cloudsmith.geppetto.pp.dsl.ui/src/org/cloudsmith/geppetto/pp/dsl/ui/builder/ToggleNatureAction.java
@@ -12,7 +12,9 @@
 package org.cloudsmith.geppetto.pp.dsl.ui.builder;
 
 import java.util.Iterator;
+import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.cloudsmith.geppetto.pp.dsl.ui.PPUiConstants;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
@@ -23,10 +25,108 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.xtext.ui.XtextProjectHelper;
+
+import com.google.inject.internal.Lists;
 
 public class ToggleNatureAction implements IObjectActionDelegate {
+	private final static Logger log = Logger.getLogger(ToggleNatureAction.class);
 
 	private ISelection selection;
+
+	private boolean turnOn;
+
+	private void addNature(IProject project) {
+		// Also add XtextNature if not already set
+		boolean addXtextNature = !XtextProjectHelper.hasNature(project);
+
+		try {
+			IProjectDescription description = project.getDescription();
+			String[] natures = description.getNatureIds();
+
+			final int newCount = 1 + (addXtextNature
+					? 1
+					: 0);
+			// Add the nature
+			String[] newNatures = new String[natures.length + newCount];
+			System.arraycopy(natures, 0, newNatures, newCount, natures.length);
+			newNatures[0] = PPUiConstants.PUPPET_NATURE_ID;
+			if(addXtextNature)
+				newNatures[1] = XtextProjectHelper.NATURE_ID;
+			description.setNatureIds(newNatures);
+			project.setDescription(description, null);
+		}
+		catch(CoreException e) {
+			log.error("Failed adding Puppet Project Nature", e);
+		}
+
+	}
+
+	/**
+	 * Returns selected accessible projects
+	 * 
+	 * @param selection
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	private List<IProject> getSelectedProjects(ISelection selection) {
+		List<IProject> projects = Lists.newArrayList();
+		if(selection instanceof IStructuredSelection) {
+			for(Iterator it = ((IStructuredSelection) selection).iterator(); it.hasNext();) {
+				Object element = it.next();
+				IProject project = null;
+				if(element instanceof IProject) {
+					project = (IProject) element;
+				}
+				else if(element instanceof IAdaptable) {
+					project = (IProject) ((IAdaptable) element).getAdapter(IProject.class);
+				}
+				if(project != null && project.isAccessible()) {
+					projects.add(project);
+				}
+			}
+		}
+		return projects;
+	}
+
+	private boolean hasNature(IProject project) {
+		try {
+			IProjectDescription description = project.getDescription();
+
+			String[] natures = description.getNatureIds();
+
+			for(int i = 0; i < natures.length; ++i)
+				if(PPUiConstants.PUPPET_NATURE_ID.equals(natures[i]))
+					return true;
+		}
+		catch(CoreException e) {
+			log.error("Error while trying to obtain nature of project", e);
+		}
+		return false;
+	}
+
+	private void removeNature(IProject project) {
+		try {
+			IProjectDescription description = project.getDescription();
+			String[] natures = description.getNatureIds();
+
+			for(int i = 0; i < natures.length; ++i) {
+				if(PPUiConstants.PUPPET_NATURE_ID.equals(natures[i])) {
+					// Remove the nature
+					String[] newNatures = new String[natures.length - 1];
+					System.arraycopy(natures, 0, newNatures, 0, i);
+					System.arraycopy(natures, i + 1, newNatures, i, natures.length - i - 1);
+					description.setNatureIds(newNatures);
+					project.setDescription(description, null);
+					return;
+				}
+			}
+		}
+		catch(CoreException e) {
+			log.error("Failed adding Puppet Project Nature", e);
+		}
+
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -60,6 +160,26 @@ public class ToggleNatureAction implements IObjectActionDelegate {
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
 		this.selection = selection;
+		List<IProject> projects = getSelectedProjects(selection);
+
+		// enable the action if at least one project is selected
+		action.setEnabled(projects.size() != 0);
+		if(projects.size() == 0) {
+			// Set a default text (for disabled state)
+			action.setText("Toggle Puppet Project Nature");
+		}
+		else {
+			// If one of the projects do not have the nature, then the action is a
+			// set nature on all
+			this.turnOn = false;
+			for(IProject p : projects)
+				if(!hasNature(p))
+					turnOn = true;
+			if(turnOn)
+				action.setText("Add Puppet Project Nature");
+			else
+				action.setText("Remove Puppet Project Nature");
+		}
 	}
 
 	/*
@@ -72,37 +192,18 @@ public class ToggleNatureAction implements IObjectActionDelegate {
 	}
 
 	/**
-	 * Toggles sample nature on a project
+	 * Toggles nature on a project
 	 * 
 	 * @param project
 	 *            to have sample nature added or removed
 	 */
 	private void toggleNature(IProject project) {
-		try {
-			IProjectDescription description = project.getDescription();
-			String[] natures = description.getNatureIds();
 
-			for(int i = 0; i < natures.length; ++i) {
-				if(PPUiConstants.PUPPET_NATURE_ID.equals(natures[i])) {
-					// Remove the nature
-					String[] newNatures = new String[natures.length - 1];
-					System.arraycopy(natures, 0, newNatures, 0, i);
-					System.arraycopy(natures, i + 1, newNatures, i, natures.length - i - 1);
-					description.setNatureIds(newNatures);
-					project.setDescription(description, null);
-					return;
-				}
-			}
-
-			// Add the nature
-			String[] newNatures = new String[natures.length + 1];
-			System.arraycopy(natures, 0, newNatures, 0, natures.length);
-			newNatures[natures.length] = PPUiConstants.PUPPET_NATURE_ID;
-			description.setNatureIds(newNatures);
-			project.setDescription(description, null);
+		if(hasNature(project)) {
+			if(!turnOn)
+				removeNature(project);
 		}
-		catch(CoreException e) {
-		}
+		else if(turnOn)
+			addNature(project);
 	}
-
 }

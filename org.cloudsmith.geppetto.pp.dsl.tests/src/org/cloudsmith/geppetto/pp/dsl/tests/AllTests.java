363f74eba4de5434c12920ac945ffc500268b4ab
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-06-27 00:57:41 +0200

    Removed unused test (old pptp test).

--- org.cloudsmith.geppetto.pp.dsl.tests/src/org/cloudsmith/geppetto/pp/dsl/tests/AllTests.java
+++ org.cloudsmith.geppetto.pp.dsl.tests/src/org/cloudsmith/geppetto/pp/dsl/tests/AllTests.java
@@ -31,7 +31,6 @@ public class AllTests {
 		suite.addTestSuite(TestSelectorExpression.class);
 		suite.addTestSuite(TestDoubleQuotedString.class);
 		suite.addTestSuite(TestIssues.class);
-		// suite.addTestSuite(PPTPManagerTests.class);
 		// $JUnit-END$
 		return suite;
 	}

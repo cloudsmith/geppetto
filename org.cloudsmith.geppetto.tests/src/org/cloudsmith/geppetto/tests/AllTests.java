da3ac34e2a4005cbcd36b0a397b03f6dfa38bfc9
thallgren <thomas@tada.se>
2011-08-25 09:17:29 +0200

    Added test execution to the build

---
+++ org.cloudsmith.geppetto.tests/src/org/cloudsmith/geppetto/tests/AllTests.java
@@ -0,0 +1,20 @@
+package org.cloudsmith.geppetto.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+
+		suite.addTest(org.cloudsmith.geppetto.forge.tests.ForgeTests.suite());
+		// suite.addTest(org.cloudsmith.geppetto.ruby.tests.AllTests.suite());
+		// suite.addTest(org.cloudsmith.geppetto.pp.dsl.tests.AllTests.suite());
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}

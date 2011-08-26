a9733b53534ebaf62a99b9c6e1bb0dcfe2e04a38
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-08-26 03:23:23 +0200

    Basic setup of catalog test

---
+++ org.cloudsmith.geppetto.catalog.test/src/org/cloudsmith/geppetto/catalog/test/AllTests.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2011 Cloudsmith Inc. and other contributors, as listed below.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Cloudsmith
+ * 
+ */
+
+package org.cloudsmith.geppetto.catalog.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * All Puppet Tests.
+ * 
+ */
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		// $JUnit-BEGIN$
+		suite.addTestSuite(TestJsonLoad.class);
+		// $JUnit-END$
+		return suite;
+	}
+
+}
---
+++ org.cloudsmith.geppetto.catalog.test/src/org/cloudsmith/geppetto/catalog/test/TestJsonLoad.java
@@ -0,0 +1,10 @@
+package org.cloudsmith.geppetto.catalog.test;
+
+import junit.framework.TestCase;
+
+public class TestJsonLoad extends TestCase {
+
+	public void testLoad() {
+		
+	}
+}

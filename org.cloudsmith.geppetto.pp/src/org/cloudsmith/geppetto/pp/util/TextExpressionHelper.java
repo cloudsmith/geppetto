a2b19fa399898db573d33be2ee04096787ad6ea3
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-12-21 04:07:27 +0100

    Cleansup dead code.

--- org.cloudsmith.geppetto.pp/src/org/cloudsmith/geppetto/pp/util/TextExpressionHelper.java
+++ org.cloudsmith.geppetto.pp/src/org/cloudsmith/geppetto/pp/util/TextExpressionHelper.java
@@ -16,61 +16,10 @@ import org.cloudsmith.geppetto.pp.TextExpression;
 import org.cloudsmith.geppetto.pp.VerbatimTE;
 
 /**
- * Tests Literals
+ * Helps with TextExpressions in interpolated Strings.
  * 
  */
 public class TextExpressionHelper {
-
-	// public static class StringData {
-	// final TextExpression te;
-	//
-	// final String string;
-	//
-	// StringData(final TextExpression firstElement, final String secondElement) {
-	// te = firstElement;
-	// string = secondElement;
-	// }
-	//
-	// public String getString() {
-	// return string;
-	// }
-	//
-	// public TextExpression getTextExpression() {
-	// return te;
-	// }
-	//
-	// boolean isExprClass(Class<?> clazz) {
-	// if(!(te instanceof ExpressionTE))
-	// return false;
-	// return clazz.isAssignableFrom(((ExpressionTE) te).getExpression().getClass());
-	// }
-	// }
-
-	// private static void flatten(List<StringData> result, TextExpression te) {
-	// if(te == null)
-	// return;
-	// if(te.getLeading() != null)
-	// flatten(result, te.getLeading());
-	// if(te instanceof VerbatimTE)
-	// result.add(stringData(te, ((VerbatimTE) te).getText()));
-	// else if(te instanceof VariableTE)
-	// result.add(stringData(te, ((VariableTE) te).getVarName()));
-	// else if(te instanceof ExpressionTE)
-	// result.add(stringData(te, "EXPRESSION"));
-	//
-	// if(te.getTrailing() != null)
-	// flatten(result, te.getTrailing());
-	// }
-
-	// private static List<StringData> flattenTextExpression(TextExpression te) {
-	// List<StringData> result = new ArrayList<StringData>();
-	// flatten(result, te);
-	// return result;
-	// }
-
-	// public static List<StringData> getStringData(DoubleQuotedString dqString) {
-	// return flattenTextExpression(dqString.getTextExpression());
-	// }
 
 	public static boolean hasInterpolation(DoubleQuotedString dqString) {
 		for(TextExpression stringPart : dqString.getStringPart())
@@ -78,16 +27,7 @@ public class TextExpressionHelper {
 				return true;
 		return false;
 
-		// List<StringData> stringData = getStringData(dqString);
-		// for(StringData sd : stringData)
-		// if(!(sd.te instanceof VerbatimTE))
-		// return true;
-		// return false;
 	}
-
-	// private static StringData stringData(TextExpression te, String s) {
-	// return new StringData(te, s);
-	// }
 
 	public String doubleQuote(String s) {
 		return '"' + s + '"';

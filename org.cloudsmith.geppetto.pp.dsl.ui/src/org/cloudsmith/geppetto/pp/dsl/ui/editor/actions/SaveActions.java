8b60836a3491e6f8b82d011017fee4296e53c58c
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2012-02-01 02:34:42 +0100

    Extends replacement of replaced "funky" spaces to full set.
    The full set are all the characters painted by the formatter.
    See class SaveActions, and PPSemanticHighlightingCalculator.

--- org.cloudsmith.geppetto.pp.dsl.ui/src/org/cloudsmith/geppetto/pp/dsl/ui/editor/actions/SaveActions.java
+++ org.cloudsmith.geppetto.pp.dsl.ui/src/org/cloudsmith/geppetto/pp/dsl/ui/editor/actions/SaveActions.java
@@ -26,9 +26,29 @@ import com.google.inject.Inject;
  * 
  */
 public class SaveActions implements ISaveActions {
-	private static Pattern trimPattern = Pattern.compile("[ \\t\\f\\x0B\\u00A0]+(\\r\\n|\\n)");
 
-	private static Pattern funkySpacePattern = Pattern.compile("[\\f\\x0B\\u00A0]");
+	// case '\u00A0': // NBSP
+	// case '\u1680': // OGHAM SPACE MARK");
+	// case '\u2000': // EN QUAD");
+	// case '\u2001': // EM QUAD");
+	// case '\u2002': // EN SPACE");
+	// case '\u2003': // EM SPACE");
+	// case '\u2004': // THREE-PER-EM SPACE");
+	// case '\u2005': // FOUR-PER-EM SPACE");
+	// case '\u2006': // SIX-PER-EM SPACE");
+	// case '\u2007': // FIGURE SPACE");
+	// case '\u2008': // PUNCTUATION SPACE");
+	// case '\u2009': // THIN SPACE");
+	// case '\u200A': // HAIR SPACE");
+	// case '\u200B': // ZERO WIDTH SPACE");
+	// case '\u202F': // NARROW NO-BREAK SPACE");
+	// case '\u3000': // IDEOGRAPHIC SPACE");
+
+	public static String funkySpaces = "\\u00A0\\u1680\\u2000\\u2001\\u2002\\u2003\\u2004\\u2005\\u2006\\u2007\\u2008\\u2009\\u200A\\u200B\\u202F\\u3000";
+
+	private static Pattern trimPattern = Pattern.compile("[ \\t\\f\\x0B" + funkySpaces + "]+(\\r\\n|\\n)");
+
+	private static Pattern funkySpacePattern = Pattern.compile("[\\f\\x0B" + funkySpaces + "]");
 
 	@Inject
 	private PPPreferencesHelper preferenceHelper;

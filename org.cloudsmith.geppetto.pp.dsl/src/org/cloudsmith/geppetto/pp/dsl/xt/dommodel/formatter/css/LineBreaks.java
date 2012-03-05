40b7b8ff628c486add5eec955d219726b2bb0320
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2012-03-05 02:22:19 +0100

    Unifies implementation of the flexible quantities spacing and linebreaks
    THe classes Spacing and LineBreaks where basically the same. A common
    superclass and interface was added. Also added apply to compute
    resulting quantity given an existing quantity.

--- org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/xt/dommodel/formatter/css/LineBreaks.java
+++ org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/xt/dommodel/formatter/css/LineBreaks.java
@@ -12,48 +12,19 @@
 package org.cloudsmith.geppetto.pp.dsl.xt.dommodel.formatter.css;
 
 /**
- * Describes desired line breaks in terms of min, max, and normal
+ * Describes desired line breaks as an {@link IFlexibleQuantity}.
  * 
  */
-public class LineBreaks {
-	private int normal;
-
-	private int max;
-
-	private int min;
-
+public class LineBreaks extends FlexibleQuantity {
 	public LineBreaks() {
-		this(1);
+		super(1);
 	}
 
 	public LineBreaks(int normal) {
-		this(normal, normal, normal);
+		super(normal, normal, normal);
 	}
 
 	public LineBreaks(int min, int normal, int max) {
-		this.min = min;
-		this.max = max;
-		this.normal = normal;
-	}
-
-	/**
-	 * @return the max
-	 */
-	public int getMax() {
-		return max;
-	}
-
-	/**
-	 * @return the min
-	 */
-	public int getMin() {
-		return min;
-	}
-
-	/**
-	 * @return the normal
-	 */
-	public int getNormal() {
-		return normal;
+		super(min, normal, max);
 	}
 }
--- org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/xt/dommodel/formatter/css/Spacing.java
+++ org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/xt/dommodel/formatter/css/Spacing.java
@@ -12,48 +12,19 @@
 package org.cloudsmith.geppetto.pp.dsl.xt.dommodel.formatter.css;
 
 /**
- * Describes desired spacing in terms of min, max, and normal
+ * Describes desired spacing as an {@link IFlexibleQuantity}.
  * 
  */
-public class Spacing {
-	private int normal;
-
-	private int max;
-
-	private int min;
-
+public class Spacing extends FlexibleQuantity {
 	public Spacing() {
-		this(1);
+		super(1);
 	}
 
 	public Spacing(int normal) {
-		this(normal, normal, normal);
+		super(normal, normal, normal);
 	}
 
 	public Spacing(int min, int normal, int max) {
-		this.min = min;
-		this.max = max;
-		this.normal = normal;
-	}
-
-	/**
-	 * @return the max
-	 */
-	public int getMax() {
-		return max;
-	}
-
-	/**
-	 * @return the min
-	 */
-	public int getMin() {
-		return min;
-	}
-
-	/**
-	 * @return the normal
-	 */
-	public int getNormal() {
-		return normal;
+		super(min, normal, max);
 	}
 }

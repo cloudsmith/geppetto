7517a5694249756ad152fe5102fb7274957e3650
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2012-03-05 02:34:58 +0100

    Adds PP indentation information (2 spaces). Adds and updates tests.
    Added/modified tests to test trailing WS, indentation, and line breaks.

--- org.cloudsmith.geppetto.pp.dsl.tests/src/org/cloudsmith/geppetto/pp/dsl/tests/TestSemanticCssFormatter.java
+++ org.cloudsmith.geppetto.pp.dsl.tests/src/org/cloudsmith/geppetto/pp/dsl/tests/TestSemanticCssFormatter.java
@@ -18,6 +18,7 @@ import org.cloudsmith.geppetto.pp.AssignmentExpression;
 import org.cloudsmith.geppetto.pp.LiteralList;
 import org.cloudsmith.geppetto.pp.PPFactory;
 import org.cloudsmith.geppetto.pp.PuppetManifest;
+import org.cloudsmith.geppetto.pp.dsl.ppformatting.PPIndentationInformation;
 import org.cloudsmith.geppetto.pp.dsl.serializer.HiddenTokenSequencerForDom;
 import org.cloudsmith.geppetto.pp.dsl.xt.dommodel.DomModelUtils;
 import org.cloudsmith.geppetto.pp.dsl.xt.dommodel.IDomNode;
@@ -45,7 +46,7 @@ import com.google.inject.Injector;
 import com.google.inject.util.Modules;
 
 /**
- * @author henrik
+ * Tests PP language formatting using the new DomFormatter.
  * 
  */
 public class TestSemanticCssFormatter extends AbstractPuppetTests {
@@ -56,13 +57,6 @@ public class TestSemanticCssFormatter extends AbstractPuppetTests {
 			super(styles, functions);
 		}
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.cloudsmith.geppetto.pp.dsl.xt.dommodel.formatter.IDomModelFormatter#format(org.cloudsmith.geppetto.pp.dsl.xt.dommodel.IDomNode,
-		 * org.eclipse.xtext.util.ITextRegion, org.cloudsmith.geppetto.pp.dsl.xt.dommodel.formatter.IFormattingContext,
-		 * org.eclipse.xtext.serializer.diagnostic.ISerializationDiagnostic.Acceptor)
-		 */
 		@Override
 		public ReplaceRegion format(IDomNode dom, ITextRegion regionToFormat, IFormattingContext formattingContext,
 				Acceptor errors) {
@@ -74,21 +68,20 @@ public class TestSemanticCssFormatter extends AbstractPuppetTests {
 
 	public static class TestSetup extends PPTestSetup {
 		public static class TestModule extends PPTestModule {
-			/*
-			 * (non-Javadoc)
-			 * 
-			 * @see org.eclipse.xtext.service.AbstractGenericModule#configure(com.google.inject.Binder)
-			 */
+
 			@Override
 			public void configure(Binder binder) {
 				super.configure(binder);
 				binder.bind(ISerializer.class).to(DomBasedSerializer.class);
-				binder.bind(IIndentationInformation.class).to(IIndentationInformation.Default.class);
 				binder.bind(IDomModelFormatter.class).to(DebugFormatter.class);
 				binder.bind(IFunctionFactory.class).to(FunctionFactory.class);
 				binder.bind(IStyleFactory.class).to(StyleFactory.class);
 				// Want serializer to insert empty WS even if there is no node model
 				binder.bind(IHiddenTokenSequencer.class).to(HiddenTokenSequencerForDom.class);
+
+				// specific to pp (2 spaces).
+				binder.bind(IIndentationInformation.class).to(PPIndentationInformation.class);
+				// binder.bind(IIndentationInformation.class).to(IIndentationInformation.Default.class);
 			}
 		}
 
@@ -108,7 +101,7 @@ public class TestSemanticCssFormatter extends AbstractPuppetTests {
 
 	public void test_Serialize_arrayNoSpaces() throws Exception {
 		String code = "$a=['10','20']";
-		String fmt = "$a = ['10', '20']";
+		String fmt = "$a = ['10', '20']\n";
 		XtextResource r = getResourceFromString(code);
 		String s = serializeFormatted(r.getContents().get(0));
 		assertEquals("serialization should produce same result", fmt, s);
@@ -116,7 +109,7 @@ public class TestSemanticCssFormatter extends AbstractPuppetTests {
 
 	public void test_Serialize_arrayWithComments() throws Exception {
 		String code = "/*1*/$a/*2*/=/*3*/[/*4*/'10'/*5*/,/*6*/'20'/*7*/]/*8*/";
-		String fmt = "/*1*/ $a /*2*/ = /*3*/ [/*4*/ '10' /*5*/, /*6*/ '20' /*7*/] /*8*/";
+		String fmt = "/*1*/ $a /*2*/ = /*3*/ [/*4*/ '10' /*5*/, /*6*/ '20' /*7*/] /*8*/\n";
 		XtextResource r = getResourceFromString(code);
 		String s = serializeFormatted(r.getContents().get(0));
 		assertEquals("serialization should produce same result", fmt, s);
@@ -131,8 +124,17 @@ public class TestSemanticCssFormatter extends AbstractPuppetTests {
 		pplist.getElements().add(createSqString("10"));
 		pplist.getElements().add(createSqString("20"));
 		pp.getStatements().add(assignment);
-		String fmt = "$a = ['10', '20']";
+		String fmt = "$a = ['10', '20']\n";
 		String s = serializeFormatted(pp);
+		assertEquals("serialization should produce same result", fmt, s);
+	}
+
+	public void test_Serialize_simpleResource() throws Exception {
+		String code = "file{'afile':owner=>'foo'}\n\n\n\n\n";
+		// this is not wanted end result, only used to test intermediate progress on indentation and linebreak
+		String fmt = "file {\n  'afile' : owner => 'foo'\n}\n\n";
+		XtextResource r = getResourceFromString(code);
+		String s = serializeFormatted(r.getContents().get(0));
 		assertEquals("serialization should produce same result", fmt, s);
 	}
 

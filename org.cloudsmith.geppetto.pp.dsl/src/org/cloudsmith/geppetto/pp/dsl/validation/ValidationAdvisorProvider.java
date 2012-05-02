995d53f797c39b3410fa5aac034f6495247c6963
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2012-05-03 00:46:04 +0200

    Adds IPotentialProblemsAdvisor parameter to startup. Closes #319

--- org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/validation/ValidationAdvisorProvider.java
+++ org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/validation/ValidationAdvisorProvider.java
@@ -20,23 +20,34 @@ import com.google.inject.Provider;
 public class ValidationAdvisorProvider<T extends IValidationAdvisor> implements Provider<IValidationAdvisor> {
 	public static <T extends IValidationAdvisor> ValidationAdvisorProvider<T> create(
 			IValidationAdvisor.ComplianceLevel level) {
-		return new ValidationAdvisorProvider<T>(level);
+		return new ValidationAdvisorProvider<T>(level, null);
+	}
+
+	public static <T extends IValidationAdvisor> ValidationAdvisorProvider<T> create(
+			IValidationAdvisor.ComplianceLevel level, IPotentialProblemsAdvisor problemsAdvisor) {
+		return new ValidationAdvisorProvider<T>(level, problemsAdvisor);
 	}
 
 	private final IValidationAdvisor.ComplianceLevel level;
 
-	public ValidationAdvisorProvider(IValidationAdvisor.ComplianceLevel level) {
+	private final IPotentialProblemsAdvisor problemsAdvisor;
+
+	public ValidationAdvisorProvider(IValidationAdvisor.ComplianceLevel level, IPotentialProblemsAdvisor problemsAdvisor) {
 		this.level = level;
+		this.problemsAdvisor = problemsAdvisor;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Provides an IValidatioNAdvisor configured from the ComplianceLevel and (optional) ProblemsAdvisor
+	 * passed to {@link #create(org.cloudsmith.geppetto.pp.dsl.validation.IValidationAdvisor.ComplianceLevel, IPotentialProblemsAdvisor)}.
 	 * 
-	 * @see com.google.inject.Provider#get()
+	 * If the problems advisor is null, an instance of {@link DefaultPotentialProblemsAdvisor} is used.
 	 */
 	@Override
 	public IValidationAdvisor get() {
-		return ValidationAdvisor.create(level, new DefaultPotentialProblemsAdvisor());
+		return ValidationAdvisor.create(level, problemsAdvisor != null
+				? problemsAdvisor
+				: new DefaultPotentialProblemsAdvisor());
 	}
 
 }

8e4f6aa4d49d9d259aab0dc9ca3005002fbb3db3
Thomas Hallgren <thomas@tada.se>
2013-03-23 07:46:17 +0100

    Removes API methods that are no longer possible to use
    Some methods in the related to user creation and manipulation has been
    made private so that only the Puppetlabs Forge web client can use them.
    This commit removes those methods from the API.

--- org.cloudsmith.geppetto.forge.api/src/main/java/org/cloudsmith/geppetto/forge/v2/service/UserService.java
+++ org.cloudsmith.geppetto.forge.api/src/main/java/org/cloudsmith/geppetto/forge/v2/service/UserService.java
@@ -13,16 +13,12 @@ package org.cloudsmith.geppetto.forge.v2.service;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpResponseException;
 import org.cloudsmith.geppetto.forge.v2.client.Constants;
-import org.cloudsmith.geppetto.forge.v2.model.ConfirmationToken;
 import org.cloudsmith.geppetto.forge.v2.model.Module;
-import org.cloudsmith.geppetto.forge.v2.model.PasswordToken;
 import org.cloudsmith.geppetto.forge.v2.model.Release;
 import org.cloudsmith.geppetto.forge.v2.model.User;
 
@@ -35,67 +31,6 @@ public class UserService extends ForgeService {
 	}
 
 	/**
-	 * Confirms the user using the given <code>confirmationToken</code>. This method will fail
-	 * unless the OAuth token is associated with the ForgeAPI Web application and the logged in
-	 * user is the owner of the given email
-	 * 
-	 * @param confirmationToken
-	 *            The token to use when confirming
-	 * @throws IOException
-	 */
-	public void confirmEmail(ConfirmationToken confirmationToken) throws IOException {
-		getClient(true).postJSON(Constants.COMMAND_GROUP_EMAIL + "/confirm", confirmationToken, null);
-	}
-
-	/**
-	 * Resets the password of the user using the given <code>passwordToken</code>. This method will fail
-	 * unless the OAuth token is associated with the ForgeAPI Web application and the logged in
-	 * user is the owner of the <code>passwordToken</code>
-	 * 
-	 * @param passwordToken
-	 *            The token to use when confirming
-	 * @throws IOException
-	 */
-	public void confirmPasswordReset(PasswordToken passwordToken, String newPassword) throws IOException {
-		Map<String, String> postData = toQueryMap(passwordToken);
-		postData.put("password", newPassword);
-		getClient(true).postJSON(Constants.COMMAND_GROUP_PASSWORD + "/reset", postData, null);
-	}
-
-	/**
-	 * Creates a new user. The given <code>name</code> must be unique since it is used to
-	 * identify the user.
-	 * 
-	 * @param username
-	 * @param email
-	 * @param displayName
-	 * @param password
-	 * @return The created user
-	 * @throws IOException
-	 *             if the user could not be created
-	 */
-	public User create(String username, String email, String displayName, String password) throws IOException {
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("username", username);
-		params.put("email", email);
-		params.put("display_name", displayName);
-		params.put("password", password);
-		return getClient(false).postJSON(Constants.COMMAND_GROUP_USERS, params, User.class);
-	}
-
-	/**
-	 * Delete a user
-	 * 
-	 * @param name
-	 *            The name of the user to delete
-	 * @throws IOException
-	 *             if the user could not be deleted
-	 */
-	public void delete(String name) throws IOException {
-		getClient(true).delete(getUserPath(name));
-	}
-
-	/**
 	 * 
 	 * @param name
 	 *            The name of the user
@@ -104,22 +39,6 @@ public class UserService extends ForgeService {
 	 */
 	public User get(String name) throws IOException {
 		return getClient(false).get(getUserPath(name), null, User.class);
-	}
-
-	/**
-	 * Retrieves confirmation token for a user, using an email address. This method will fail
-	 * unless the OAuth token is associated with the ForgeAPI Web application and the logged in
-	 * user is the owner of the given email
-	 * 
-	 * @param email
-	 *            The users email address
-	 * @return The confirmation token
-	 * @throws IOException
-	 */
-	public ConfirmationToken getConfirmationToken(String email) throws IOException {
-		return getClient(true).get(
-			Constants.COMMAND_GROUP_EMAIL + "/confirm", Collections.singletonMap("email", email),
-			ConfirmationToken.class);
 	}
 
 	/**
@@ -143,21 +62,6 @@ public class UserService extends ForgeService {
 		if(modules == null)
 			modules = Collections.emptyList();
 		return modules;
-	}
-
-	/**
-	 * Retrieves password reset token for a user, using an email address. This method will fail
-	 * unless the OAuth token is associated with the ForgeAPI Web application and the logged in
-	 * user is the owner of the given email
-	 * 
-	 * @param email
-	 *            The users email address
-	 * @return The password reset token
-	 * @throws IOException
-	 */
-	public PasswordToken getPasswordToken(String email) throws IOException {
-		return getClient(true).get(
-			Constants.COMMAND_GROUP_PASSWORD + "/reset", Collections.singletonMap("email", email), PasswordToken.class);
 	}
 
 	/**
@@ -201,27 +105,5 @@ public class UserService extends ForgeService {
 		if(users == null)
 			users = Collections.emptyList();
 		return users;
-	}
-
-	/**
-	 * Updates the user identified with <code>currentName</code>.
-	 * 
-	 * @param currentName
-	 *            The current name of the user
-	 * @param newName
-	 *            The new name of the user
-	 * @param email
-	 *            The new email for the user
-	 * @param displayName
-	 *            The new display name for the user
-	 * @param password
-	 *            The users new password
-	 * @return The updated user
-	 * @throws IOException
-	 */
-	public User update(String currentName, User user, String password) throws IOException {
-		Map<String, String> params = toQueryMap(user);
-		params.put("password", password);
-		return getClient(true).patch(getUserPath(currentName), params, User.class);
 	}
 }

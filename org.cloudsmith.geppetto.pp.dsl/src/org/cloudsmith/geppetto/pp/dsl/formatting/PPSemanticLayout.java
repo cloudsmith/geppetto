0bf1ee07d9ecfb82e9e167d6852958374d06ec5f
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-04-19 18:27:42 +0200

    Add formatting of Lambdas.
    This adds formatting of lambdas. If a lambda contains a newline
    before the closing } it will be rendered on multiple lines. Otherwise
    an attempt is made to render it in place (compact form). If the compact
    form does not fit on the remainder of the line the lambda is rendered
    using non compact form.

--- org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/formatting/PPSemanticLayout.java
+++ org.cloudsmith.geppetto.pp.dsl/src/org/cloudsmith/geppetto/pp/dsl/formatting/PPSemanticLayout.java
@@ -13,6 +13,7 @@ package org.cloudsmith.geppetto.pp.dsl.formatting;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.cloudsmith.geppetto.pp.AppendExpression;
 import org.cloudsmith.geppetto.pp.AssignmentExpression;
@@ -54,6 +55,7 @@ import org.cloudsmith.xtext.dommodel.formatter.css.StyleSet;
 import org.cloudsmith.xtext.textflow.ITextFlow;
 import org.cloudsmith.xtext.textflow.MeasuredTextFlow;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.util.Tuples;
@@ -113,9 +115,14 @@ public class PPSemanticLayout extends DeclarativeSemanticFlowLayout {
 		BLOCK,
 
 		/**
-		 * Can be rendered in compact form
+		 * May be rendered in compact form
 		 */
-		COMPACTABLE;
+		COMPACTABLE,
+
+		/**
+		 * Render inline
+		 */
+		INLINE;
 	}
 
 	@Inject
@@ -144,6 +151,9 @@ public class PPSemanticLayout extends DeclarativeSemanticFlowLayout {
 
 	@Inject
 	private Provider<IBreakAndAlignAdvice> adviceProvider;
+
+	@Inject
+	LayoutUtils layoutUtils;
 
 	/**
 	 * array of classifiers that represent {@code org.cloudsmith.geppetto.pp.dsl.formatting.PPSemanticLayout.StatementStyle.BLOCK} - used for fast
@@ -252,9 +262,10 @@ public class PPSemanticLayout extends DeclarativeSemanticFlowLayout {
 	}
 
 	protected boolean _format(JavaLambda o, StyleSet styleSet, IDomNode node, ITextFlow flow, ILayoutContext context) {
-		internalFormatStatementList(
-			node, grammarAccess.getJava8LambdaAccess().getStatementsExpressionListParserRuleCall_6_0());
-		return false;
+		AbstractElement statements = grammarAccess.getJava8LambdaAccess().getStatementsExpressionListParserRuleCall_6_0();
+		AbstractElement fromElement = grammarAccess.getJava8LambdaAccess().getVerticalLineKeyword_0();
+		AbstractElement toElement = grammarAccess.getJava8LambdaAccess().getRightCurlyBracketKeyword_7();
+		return internalFormatLambda(node, flow, context, statements, fromElement, toElement);
 	}
 
 	protected boolean _format(LiteralHash o, StyleSet styleSet, IDomNode node, ITextFlow flow, ILayoutContext context) {
@@ -321,9 +332,10 @@ public class PPSemanticLayout extends DeclarativeSemanticFlowLayout {
 	}
 
 	protected boolean _format(RubyLambda o, StyleSet styleSet, IDomNode node, ITextFlow flow, ILayoutContext context) {
-		internalFormatStatementList(
-			node, grammarAccess.getRubyLambdaAccess().getStatementsExpressionListParserRuleCall_5_0());
-		return false;
+		AbstractElement statements = grammarAccess.getRubyLambdaAccess().getStatementsExpressionListParserRuleCall_5_0();
+		AbstractElement fromElement = grammarAccess.getRubyLambdaAccess().getLAMBDATerminalRuleCall_0();
+		AbstractElement toElement = grammarAccess.getRubyLambdaAccess().getRightCurlyBracketKeyword_6();
+		return internalFormatLambda(node, flow, context, statements, fromElement, toElement);
 	}
 
 	protected boolean _format(SelectorExpression se, StyleSet styleSet, IDomNode node, ITextFlow flow,
@@ -352,6 +364,38 @@ public class PPSemanticLayout extends DeclarativeSemanticFlowLayout {
 	protected boolean _format(VerbatimTE o, StyleSet styleSet, IDomNode node, ITextFlow flow, ILayoutContext context) {
 		flow.appendText(o.getText(), true);
 		return true;
+	}
+
+	protected void changeInlineStyle(IDomNode node, boolean set) {
+		List<IDomNode> nodes = node.getChildren();
+		Iterator<IDomNode> itor = nodes.iterator();
+		while(itor.hasNext()) {
+			IDomNode n = itor.next();
+			IDomNode firstToken = firstSignificantNode(n);
+			if(firstToken == null) {
+				continue;
+			}
+			Set<Object> styleClassifiers = firstToken.getStyleClassifiers();
+			if(set)
+				styleClassifiers.add(StatementStyle.INLINE);
+			else
+				styleClassifiers.remove(StatementStyle.INLINE);
+		}
+	}
+
+	/**
+	 * Returns true if there is source text and this source contains a line break (before formatting) before a closing '}'
+	 * 
+	 * @param node
+	 * @return true if the node contains a line break anywhere it its complete text.
+	 */
+	protected boolean containsEndLineBreak(IDomNode node) {
+		INode n = node.getNode();
+		// ?s: means dotall (. matches \n)
+		// *? means non greedy since we need to check if there is a \n before the }
+		// optional space \s between last newline and }
+		//
+		return n != null && n.getText().matches("(?s:.*?)\\n\\s*\\}$");
 	}
 
 	/**
@@ -383,6 +427,27 @@ public class PPSemanticLayout extends DeclarativeSemanticFlowLayout {
 			}
 		}
 		return firstToken;
+	}
+
+	protected boolean internalFormatLambda(IDomNode node, ITextFlow flow, ILayoutContext context,
+			AbstractElement statements, AbstractElement fromElement, AbstractElement toElement) {
+		boolean hasLineBreak = containsEndLineBreak(node);
+		Pair<Integer, Integer> counts = internalFormatStatementList(node, statements);
+
+		// A lambda is compactable unless it contains a linebreak, and unless it contains block expressions (that always break the line)
+		boolean canBeCompacted = !hasLineBreak && counts.getSecond() < 1;
+		if(canBeCompacted) {
+			node.getStyleClassifiers().add(StatementStyle.COMPACTABLE);
+			// Make all inline and measure if it fits (if not, revoke the inline style
+			changeInlineStyle(node, true);
+			// Measure only the lambda
+			if(!layoutUtils.fitsOnSameLine(node, fromElement, toElement, flow, context))
+				changeInlineStyle(node, false);
+		}
+		else
+			changeInlineStyle(node, false);
+		return false;
+
 	}
 
 	/**

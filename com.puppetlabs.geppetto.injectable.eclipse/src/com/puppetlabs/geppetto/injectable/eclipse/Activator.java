8b454f8fab3366ef4c783a3c1bdd33ca9b5a4195
Thomas Hallgren <thomas@tada.se>
2013-09-04 14:22:29 +0200

    Add conversion of 'org.cloudsmith' bound bundle states and preferences
    This commit prevents that a workspace that is created with a Geppetto
    that used the 'org.cloudsmith' namespace looses its preferences when
    switching to the new 'com.puppetlabs' namespace. Three steps are added
    to ensure this:
    
    1. Rename bundle state directories under .metadata/.plugins
    2. Rename .prefs files in .metadata/.plugins/org.eclipse.core.runtime
    3. Perform string substitution in renamed prefs files

--- com.puppetlabs.geppetto.injectable.eclipse/src/com/puppetlabs/geppetto/injectable/eclipse/Activator.java
+++ com.puppetlabs.geppetto.injectable.eclipse/src/com/puppetlabs/geppetto/injectable/eclipse/Activator.java
@@ -3,7 +3,16 @@
  */
 package com.puppetlabs.geppetto.injectable.eclipse;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+
 import org.eclipse.core.net.proxy.IProxyService;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -28,6 +37,84 @@ public class Activator implements BundleActivator {
 
 	private IProxyService proxyService;
 
+	private static final String ORG_CLOUDSMITH = "org.cloudsmith.";
+
+	private static final String COM_PUPPETLABS = "com.puppetlabs.";
+
+	// This is a hack to preserve workspaces created with a Geppetto that used the 'org.cloudsmith.'
+	// package and bundle naming. It ensures that the bundle states and workspace preferences are
+	// changed accordingly.
+	//
+	private static void renameCloudsmithPrefs(Bundle bundle) {
+		try {
+			IPath bundleStateRoot = Platform.getLocation().append(".metadata").append(".plugins");
+			File[] bundleDirs = bundleStateRoot.toFile().listFiles();
+			if(bundleDirs == null) {
+				System.out.format("%s is not a directory\n", bundleStateRoot.toOSString());
+				return;
+			}
+
+			for(File bundleDir : bundleDirs) {
+				String name = bundleDir.getName();
+				if(!name.startsWith(ORG_CLOUDSMITH))
+					continue;
+
+				String newName = COM_PUPPETLABS + name.substring(ORG_CLOUDSMITH.length());
+				File newDir = new File(bundleDir.getParentFile(), newName);
+				if(!newDir.exists()) {
+					bundleDir.renameTo(newDir);
+					System.out.format("Renamed %s to %s\n", bundleDir.getAbsolutePath(), newName);
+				}
+			}
+
+			IPath settingsRoot = bundleStateRoot.append("org.eclipse.core.runtime").append(".settings");
+			File[] prefsFiles = settingsRoot.toFile().listFiles();
+			if(prefsFiles == null) {
+				System.out.format("%s is not a directory\n", settingsRoot.toOSString());
+				return;
+			}
+
+			for(File prefsFile : prefsFiles) {
+				String name = prefsFile.getName();
+				if(!name.startsWith(ORG_CLOUDSMITH))
+					continue;
+
+				String changed = "";
+				String newName = COM_PUPPETLABS + name.substring(ORG_CLOUDSMITH.length());
+				File newFile = new File(prefsFile.getParentFile(), newName);
+				Reader reader = new FileReader(prefsFile);
+				try {
+					StringBuilder bld = new StringBuilder();
+					char[] buf = new char[1024];
+					int count;
+					while((count = reader.read(buf)) > 0)
+						bld.append(buf, 0, count);
+					int nxt = bld.indexOf(ORG_CLOUDSMITH);
+					while(nxt >= 0) {
+						bld.replace(nxt, nxt + ORG_CLOUDSMITH.length(), COM_PUPPETLABS);
+						nxt = bld.indexOf(ORG_CLOUDSMITH, nxt + ORG_CLOUDSMITH.length());
+						changed = " and perfomed file substitutions";
+					}
+					Writer writer = new FileWriter(newFile);
+					try {
+						writer.write(bld.toString());
+					}
+					finally {
+						writer.close();
+					}
+				}
+				finally {
+					reader.close();
+				}
+				System.out.format("Renamed %s to %s%s\n", prefsFile.getAbsolutePath(), newName, changed);
+				prefsFile.delete();
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public synchronized IProxyService getProxyService() {
 		if(proxyServiceReference == null) {
 			proxyServiceReference = context.getServiceReference(IProxyService.class);
@@ -38,6 +125,8 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
+		renameCloudsmithPrefs(context.getBundle());
+
 		this.context = context;
 		instance = this;
 	}
